94748c59db09e9262b40e57890323f5f7834244a
philipleethree <philipleethree@ff72b304-07c1-11de-b8f3-b9fb9eb71b04>
2009-03-05 10:34:42 +0000

    Test svn :-)

---
+++ trunk/mini-pie/src/org/net9/minipie/MiniPie.java
@@ -0,0 +1,13 @@
+package org.net9.minipie;
+
+public class MiniPie {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// just a test :-)
+	}
+
+}
