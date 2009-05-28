f78ee9c65974bcb48064276511576e374e072e7d
S.Stone06 <S.Stone06@ff72b304-07c1-11de-b8f3-b9fb9eb71b04>
2009-05-28 14:50:57 +0000

    提供认证所需接口，实现在UserAuth类中

---
+++ server/trunk/src/org/net9/minipie/server/logic/UserAuth.java
@@ -0,0 +1,54 @@
+/**
+ * IllegalUser.java
+ *     in package: * org.net9.minipie.server.logic
+ * by Mini-Pie Project
+ */
+package org.net9.minipie.server.logic;
+
+import org.net9.minipie.server.db.HibernateDAOFactory;
+import org.net9.minipie.server.exception.NotFoundException;
+import org.net9.minipie.server.logic.storage.UserStorage;
+
+/**
+ * @author Seastar
+ *
+ */
+public class UserAuth {
+	private String name;
+	private String pwd;
+	/**
+	 * Constructor
+	 * @param name
+	 * @param pwd
+	 */
+	public UserAuth(String name, String pwd) {
+		super();
+		this.name = name;
+		this.pwd = pwd;
+	}	
+	
+	public UserAuth(String name){
+		super();
+		this.name=name;
+	}
+	
+	public String getPassword(){
+		UserStorage executor=new HibernateDAOFactory().getUserStorage();
+		try {
+			return executor.selectUserPassword(name);
+		}catch (NotFoundException e){
+			return null;
+		}
+		
+	}
+	public long islegalUser(){
+		UserStorage executor=new HibernateDAOFactory().getUserStorage();
+		try{
+			return executor.selectLegalUser(name, pwd);
+		}catch (NotFoundException e){
+			return -1;
+		}
+		//return false;
+	}
+	
+}
