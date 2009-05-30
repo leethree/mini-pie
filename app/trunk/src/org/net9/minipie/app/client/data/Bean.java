7d783d06e775b0f3ec9e814386de3dfbad6c3b9f
philipleethree <philipleethree@ff72b304-07c1-11de-b8f3-b9fb9eb71b04>
2009-05-30 18:50:28 +0000

    commit missing files

---
+++ app/trunk/src/org/net9/minipie/app/client/data/Bean.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package org.net9.minipie.app.client.data;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author LeeThree
+ * 
+ */
+@SuppressWarnings("serial")
+public abstract class Bean implements Serializable {
+
+	public long id;
+	public String beanName;
+
+	public Map<String, String> properties = new HashMap<String, String>();
+
+	public Bean() {
+	}
+
+	public String get(String key) {
+		return properties.get(key);
+	}
+
+	/**
+	 * @return the beanName
+	 */
+	public String getBeanName() {
+		return beanName;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+}
---
+++ app/trunk/src/org/net9/minipie/app/client/data/GenericBean.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package org.net9.minipie.app.client.data;
+
+/**
+ * @author LeeThree
+ *
+ */
+@SuppressWarnings("serial")
+public class GenericBean extends Bean {
+
+}
