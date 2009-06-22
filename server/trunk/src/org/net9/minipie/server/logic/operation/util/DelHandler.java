629ffc8bb1ea00d6e8ea367b541f664f9c0b1bbb
S.Stone06 <S.Stone06@ff72b304-07c1-11de-b8f3-b9fb9eb71b04>
2009-06-22 10:06:06 +0000

    修改issue64的bug

--- server/trunk/src/org/net9/minipie/server/logic/operation/util/DelHandler.java
+++ server/trunk/src/org/net9/minipie/server/logic/operation/util/DelHandler.java
@@ -8,64 +8,78 @@ package org.net9.minipie.server.logic.operation.util;
 import org.net9.minipie.server.data.api.Delete;
 import org.net9.minipie.server.data.api.Update;
 import org.net9.minipie.server.exception.DataFormatException;
+import org.net9.minipie.server.exception.InvalidRequestException;
+import org.net9.minipie.server.exception.NotFoundException;
 import org.net9.minipie.server.exception.PermissionDeniedException;
 import org.net9.minipie.server.logic.exception.UpdateException;
 import org.net9.minipie.server.logic.storage.ContactStorage;
 
 /**
  * @author Seastar
- *
+ * 
  */
-public class DelHandler extends UpdateHandler{
+public class DelHandler extends UpdateHandler {
 
 	/**
 	 * Constructor
-	 * @throws UpdateException 
+	 * 
+	 * @throws UpdateException
 	 */
-	protected DelHandler(Update dt,ContactStorage executor,long id) throws UpdateException {
-		super(new EditHandler(dt,executor,id), dt,executor,id);
+	protected DelHandler(Update dt, ContactStorage executor, long id)
+			throws UpdateException {
+		super(new EditHandler(dt, executor, id), dt, executor, id);
 	}
-	
-	public void handleUpdate() throws UpdateException, DataFormatException{
-		if(dt instanceof Delete){
+
+	public void handleUpdate() throws UpdateException, DataFormatException {
+		if (dt instanceof Delete) {
 			Delete newData = (Delete) dt;
-			
-			switch (newData.getType()) {
+			try {
+				switch (newData.getType()) {
 				case ADDRESS:
-					if(contactExecutor.findAddressOwner(newData.getId())==contactId)			
+					if (contactExecutor.findAddressOwner(newData.getId()) == contactId)
 						contactExecutor.delAddr(newData.getId());
 					else
-						throw new PermissionDeniedException("this is not your address info");
+						throw new PermissionDeniedException(
+								"this is not your address info");
 					break;
 				case EMAIL:
-					if(contactExecutor.findEmailOwner(newData.getId())==contactId)			
+					if (contactExecutor.findEmailOwner(newData.getId()) == contactId)
 						contactExecutor.delEmail(newData.getId());
 					else
-						throw new PermissionDeniedException("this is not your email info");
+						throw new PermissionDeniedException(
+								"this is not your email info");
 					break;
 				case IM:
-					if(contactExecutor.findIMOwner(newData.getId())==contactId)			
+					if (contactExecutor.findIMOwner(newData.getId()) == contactId)
 						contactExecutor.delIM(newData.getId());
 					else
-						throw new PermissionDeniedException("this is not your im info");
+						throw new PermissionDeniedException(
+								"this is not your im info");
 					break;
 				case PHONE:
-					if(contactExecutor.findTelOwner(newData.getId())==contactId)			
+					if (contactExecutor.findTelOwner(newData.getId()) == contactId)
 						contactExecutor.delTel(newData.getId());
 					else
-						throw new PermissionDeniedException("this is not your phone info");
+						throw new PermissionDeniedException(
+								"this is not your phone info");
 					break;
 				case URL:
-					if(contactExecutor.findURLOwner(newData.getId())==contactId)			
+					if (contactExecutor.findURLOwner(newData.getId()) == contactId)
 						contactExecutor.delURL(newData.getId());
 					else
-						throw new PermissionDeniedException("this is not your url info");
+						throw new PermissionDeniedException(
+								"this is not your url info");
 					break;
 				default:
-					throw new UpdateException("Can't delete "+ newData.getType().toString());
+					throw new UpdateException("Can't delete "
+							+ newData.getType().toString());
+				}
+			} catch (NotFoundException e) {
+				throw new InvalidRequestException("no such "
+						+ newData.getType().toString() + " with the id "
+						+ newData.getId());
 			}
-		}
-		else{
+		} else {
 			super.handleUpdate();
 		}
 	}
--- server/trunk/src/org/net9/minipie/server/logic/operation/util/UserDelHandler.java
+++ server/trunk/src/org/net9/minipie/server/logic/operation/util/UserDelHandler.java
@@ -8,18 +8,22 @@ package org.net9.minipie.server.logic.operation.util;
 import org.net9.minipie.server.data.api.Delete;
 import org.net9.minipie.server.data.api.Update;
 import org.net9.minipie.server.exception.DataFormatException;
+import org.net9.minipie.server.exception.InvalidRequestException;
+import org.net9.minipie.server.exception.NotFoundException;
 import org.net9.minipie.server.exception.PermissionDeniedException;
 import org.net9.minipie.server.logic.exception.UpdateException;
 import org.net9.minipie.server.logic.storage.UserStorage;
 
 /**
  * @author Seastar
- *
+ * 
  */
-public class UserDelHandler extends UpdateHandler{
+public class UserDelHandler extends UpdateHandler {
 	private long userId;
+
 	/**
 	 * Constructor
+	 * 
 	 * @param dt
 	 * @param executor
 	 * @param id
@@ -27,51 +31,64 @@ public class UserDelHandler extends UpdateHandler{
 	 */
 	public UserDelHandler(Update dt, UserStorage executor, long id)
 			throws UpdateException {
-		super(null,dt, executor);
-		this.userId=id;
+		super(null, dt, executor);
+		this.userId = id;
 	}
 
-	public void handleUpdate() throws UpdateException, DataFormatException{
-		if(dt instanceof Delete){
+	public void handleUpdate() throws UpdateException, DataFormatException {
+		if (dt instanceof Delete) {
 			Delete newData = (Delete) dt;
-			
-			switch (newData.getType()) {
+			try {
+				switch (newData.getType()) {
+
 				case ADDRESS:
-					if(userExecutor.findAddressOwner(newData.getId())==userId)			
+					if (userExecutor.findAddressOwner(newData.getId()) == userId)
 						userExecutor.delAddr(newData.getId());
 					else
-						throw new PermissionDeniedException("this is not your address info");
+						throw new PermissionDeniedException(
+								"this is not your address info");
 					break;
 				case EMAIL:
-					if(userExecutor.findEmailOwner(newData.getId())==userId)			
+					if (userExecutor.findEmailOwner(newData.getId()) == userId)
 						userExecutor.delEmail(newData.getId());
 					else
-						throw new PermissionDeniedException("this is not your email info");
+						throw new PermissionDeniedException(
+								"this is not your email info");
 					break;
 				case IM:
-					if(userExecutor.findIMOwner(newData.getId())==userId)			
+					if (userExecutor.findIMOwner(newData.getId()) == userId)
 						userExecutor.delIM(newData.getId());
 					else
-						throw new PermissionDeniedException("this is not your im info");
+						throw new PermissionDeniedException(
+								"this is not your im info");
 					break;
 				case PHONE:
-					if(userExecutor.findTelOwner(newData.getId())==userId)			
+					if (userExecutor.findTelOwner(newData.getId()) == userId)
 						userExecutor.delTel(newData.getId());
 					else
-						throw new PermissionDeniedException("this is not your phone info");
+						throw new PermissionDeniedException(
+								"this is not your phone info");
 					break;
 				case URL:
-					if(userExecutor.findURLOwner(newData.getId())==userId)			
+					if (userExecutor.findURLOwner(newData.getId()) == userId)
 						userExecutor.delURL(newData.getId());
 					else
-						throw new PermissionDeniedException("this is not your url info");
+						throw new PermissionDeniedException(
+								"this is not your url info");
 					break;
 				default:
-					throw new UpdateException("Can't delete "+ newData.getType().toString());
+					throw new UpdateException("Can't delete "
+							+ newData.getType().toString());
+
+				}
+			} catch (NotFoundException e) {
+				throw new InvalidRequestException("no such "
+						+ newData.getType().toString() + " with the id "
+						+ newData.getId());
 			}
-		}
-		else{
+		} else {
 			super.handleUpdate();
 		}
+
 	}
 }
