af521b52f63e28d8d74be1b07e082c702f28f6b0
S.Stone06 <S.Stone06@ff72b304-07c1-11de-b8f3-b9fb9eb71b04>
2009-06-25 19:42:58 +0000

    完成两个group 功能中缺失的函数：remove user、cancel share contact

---
+++ server/trunk/src/org/net9/minipie/server/logic/operation/group/CancelSharedContact.java
@@ -0,0 +1,73 @@
+/**
+ * CancelSharedContact.java
+ *     in package: * org.net9.minipie.server.logic.operation.group
+ * by Mini-Pie Project
+ */
+package org.net9.minipie.server.logic.operation.group;
+
+import org.net9.minipie.server.data.Formatter;
+import org.net9.minipie.server.data.entity.ContactEntity;
+import org.net9.minipie.server.data.field.InfoField;
+import org.net9.minipie.server.exception.DataFormatException;
+import org.net9.minipie.server.exception.InvalidRequestException;
+import org.net9.minipie.server.exception.NotFoundException;
+import org.net9.minipie.server.exception.PermissionDeniedException;
+import org.net9.minipie.server.logic.operation.Command;
+import org.net9.minipie.server.logic.storage.ContactStorage;
+import org.net9.minipie.server.logic.storage.GroupStorage;
+import org.net9.minipie.server.logic.storage.Group_UserStorage;
+
+/**
+ * @author Seastar
+ * 
+ */
+public class CancelSharedContact extends Command<Void> {
+	private long groupId;
+	private long userId;
+	private long targetId;
+
+	public CancelSharedContact(long userId, long groupId, long targetId) {
+		this.userId = userId;
+		try {
+			this.groupId = Formatter.checkId(groupId);
+		} catch (DataFormatException e) {
+			throw new InvalidRequestException(e);
+		}
+		try {
+			this.targetId = Formatter.checkId(targetId);
+		} catch (DataFormatException e) {
+			throw new InvalidRequestException(e);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.net9.minipie.server.logic.operation.Command#execute()
+	 */
+	@Override
+	public Void execute() {
+		GroupStorage executor = getStorageFactory().getGroupStorage();
+		Group_UserStorage executor2 = getStorageFactory()
+				.getGroup_UserStorage();
+		ContactStorage executor3 = getStorageFactory().getContactStorage();
+		executor.selectGroup(groupId);
+		ContactEntity contact = executor3.selectBasicInfo(targetId).getEntity();
+		if (contact.getOwnerId() != userId)
+			throw new PermissionDeniedException("not your contact");
+		else if (contact.getGroupId() != groupId)
+			throw new InvalidRequestException(
+					"the contact has not be shared into the group");
+		else {
+			try {
+				executor2.isAdmin(userId, groupId);
+				executor3.editBasicInfo(targetId, InfoField.GROUPOWNER, 0);
+			} catch (NotFoundException e) {
+				throw new InvalidRequestException(
+						"you have not joined such group yet");
+			}
+		}
+		return null;
+	}
+
+}
---
+++ server/trunk/src/org/net9/minipie/server/logic/operation/group/admin/RemoveUser.java
@@ -0,0 +1,68 @@
+/**
+ * RemoveUser.java
+ *     in package: * org.net9.minipie.server.logic.operation.group.admin
+ * by Mini-Pie Project
+ */
+package org.net9.minipie.server.logic.operation.group.admin;
+
+import org.net9.minipie.server.data.Formatter;
+import org.net9.minipie.server.exception.DataFormatException;
+import org.net9.minipie.server.exception.InvalidRequestException;
+import org.net9.minipie.server.exception.NotFoundException;
+import org.net9.minipie.server.exception.PermissionDeniedException;
+import org.net9.minipie.server.logic.operation.Command;
+import org.net9.minipie.server.logic.storage.GroupStorage;
+import org.net9.minipie.server.logic.storage.Group_UserStorage;
+
+/**
+ * @author Seastar
+ * 
+ */
+public class RemoveUser extends Command<Void> {
+	private long groupId;
+	private long userId;
+	private long targetId;
+
+	public RemoveUser(long userId, long groupId, long targetId) {
+		this.userId = userId;
+		try {
+			this.groupId = Formatter.checkId(groupId);
+		} catch (DataFormatException e) {
+			throw new InvalidRequestException(e);
+		}
+		try {
+			this.targetId = Formatter.checkId(targetId);
+		} catch (DataFormatException e) {
+			throw new InvalidRequestException(e);
+		}
+	} /*
+		 * (non-Javadoc) *
+		 * 
+		 * @see org.net9.minipie.server.logic.operation.Command#execute()
+		 */
+
+	@Override
+	public Void execute() {
+		GroupStorage executor = getStorageFactory().getGroupStorage();
+		Group_UserStorage executor2 = getStorageFactory()
+				.getGroup_UserStorage();
+		executor.selectGroup(groupId);
+		try {
+			if (executor2.isAdmin(userId, groupId))
+				try {
+					executor2.isAdmin(targetId, groupId);
+					executor2.remove(groupId, targetId);
+				} catch (NotFoundException e) {
+					throw new InvalidRequestException(
+							"The user has not joined such group yet");
+				}
+			else
+				throw new PermissionDeniedException(
+						"you are not group administor");
+		} catch (NotFoundException e) {
+			throw new InvalidRequestException(
+					"you have not joined such group yet");
+		}
+		return null;
+	}
+}
